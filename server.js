9d640f6e93cfa6fa40061d8941f541a23ef232f3
Abhinav Joshi <abhinavjoshi1798@gmail.com>
2022-12-02 13:37:21 +0530

    products added

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ server.use(router);
 server.listen(port,()=>{
     console.log("server is running successfully");
 });
+/*
+1. git init
+2. git remote add origin link(repo link)
+3. git add .
+4. git commit -m "add_your_commit_message_here"
+5. git pull origin branchname --allow-unrelated-histories
+6. git commit -m "msg"
+7. git push origin branchname
+*/
